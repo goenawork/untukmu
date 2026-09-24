62fe194691ffbb09f93ba7a296aa3089773c5413
goenawork <goenawork@gmail.com>
2026-09-24 19:00:26 +0700

    chapters.js

--- js/chapters.js
+++ js/chapters.js
@@ -1,6 +1,6 @@
 const chapters = [
   {
-    "Saat kamu lahir"
+    "Hari saat kamu lahir.",
     year: 2024,
     title: Tahun pertamamu.,
     text: Tulis ceritanya di sini. Paragraf kedua dipisah dengan baris kosong.,
