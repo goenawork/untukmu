d29773a598461506f99cc37004417122e08ef1b6
goenawork <goenawork@gmail.com>
2026-09-24 19:08:01 +0700

    chapters.js

--- js/chapters.js
+++ js/chapters.js
@@ -9,29 +9,36 @@ Paragraf kedua dipisah dengan baris kosong.`,
     image: ""
   },
   {
-    age: 2,
+    age: 1,
     year: 2025,
+    title: "Tahun pertama.",
+    text: `Isi cerita usia dua.`,
+    image: ""
+  },
+  {
+    age: 2,
+    year: 2026,
     title: "Saat usiamu dua tahun.",
     text: `Isi cerita usia dua.`,
     image: ""
   },
   {
     age: 3,
-    year: 2026,
+    year: 2027,
     title: "Tiga tahun usiamu.",
     text: `Isi cerita usia tiga.`,
     image: ""
   },
   {
     age: 4,
-    year: 2027,
+    year: 2028,
     title: "Empat tahun pertumbuhanmu.",
     text: `Isi cerita usia empat.`,
     image: ""
   },
    {
     age: 5,
-    year: 2028,
+    year: 2029,
     title: "Lima tahun pertama.",
     text: `Isi cerita usia empat.`,
     image: ""
