c1d39c0f9f984b9393cad5f5a4ba428f5e2e7727
goenawork <goenawork@gmail.com>
2026-09-24 14:55:15 +0700

    chapters.js

--- js/chapters.js
+++ js/chapters.js
@@ -2,43 +2,43 @@ const chapters = [
   {
     Ketika kamu lahir,
     year: 2024,
-    title: "Tahun pertamamu.",
-    text: "Tulis ceritanya di sini. Paragraf kedua dipisah dengan baris kosong.",
+    title: Tahun pertamamu.,
+    text: Tulis ceritanya di sini. Paragraf kedua dipisah dengan baris kosong.,
     image: "images/usia-01.jpg" // boleh dikosongkan: image: ""
   },
   {
     Saat usiamu dua tahun,
     year: 2025,
-    title: "Judul usia dua.",
-    text: "Isi cerita usia dua.",
+    title: Judul usia dua.,
+    text: Isi cerita usia dua.,
     image: ""
   },
   {
     Tiga tahun usiamu,
     year: 2026,
-    title: "Judul usia dua.",
-    text: "Isi cerita usia dua.",
+    title: Judul usia dua.,
+    text: Isi cerita usia dua.,
     image: ""
   },
   {
     Empat tahun pertumbuhanmu,
     year: 2027,
-    title: "Judul usia dua.",
-    text: "Isi cerita usia dua.",
+    title: Judul usia dua.,
+    text: Isi cerita usia dua.,
     image: ""
   },
   {
     Lima tahun awal,
     year: 2028,
-    title: "Judul usia dua.",
-    text: "Isi cerita usia dua.",
+    title: Judul usia dua.,
+    text: Isi cerita usia dua.,
     image: ""
   },
   {
     Enam tahun,
     year: 2029,
-    title: "Judul usia dua.",
-    text: "Isi cerita usia dua.",
+    title: Judul usia dua.,
+    text: Isi cerita usia dua.,
     image: ""
   },
 ];
