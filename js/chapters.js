40a595682c62d9673f92e4d3286ffb8eb43c85f4
goenawork <goenawork@gmail.com>
2026-09-26 19:02:45 +0700

    chapters.js

--- js/chapters.js
+++ js/chapters.js
@@ -3,8 +3,7 @@ const chapterData = [
     age: 0,
     year: 2024,
     title: "Hari saat kamu lahir.",
-    text: `Dan Nala adalah namamu, sebuah arti "jantung hati" dalam bahasa Sanskerta. Lahir dari seorang ibu yang tangguh dan berpendirian teguh. Pada senin, 18 Maret, Ramadan hari ke tujuh, manakala langit pagi tengah membiru dengan syahdu.
-Kelahiranmu ditandai dengan hari yang kembali cerah setelah sepekan ke belakang hujan badai menyertai hampir di sepanjang hari.
+    text: `Dan Nala adalah namamu, sebuah arti "jantung hati" dalam bahasa Sanskerta. Lahir dari seorang ibu yang tangguh dan berpendirian teguh. Pada senin, 18 Maret, Ramadan hari ke tujuh, manakala langit pagi tengah membiru dengan syahdu. Kelahiranmu ditandai dengan hari yang kembali cerah setelah sepekan ke belakang hujan badai menyertai hampir di sepanjang hari.
 
 Pada setiap 18 Maret, selain akan kita peringati sebagai hari kelahiranmu, akan kita peringati juga sebagai hari pengorbanan ibu yang bertaruh nyawa untuk melahirkanmu ke bumi ini. Maka hari lahirmu adalah hari berterima kasih kepada ibu atas segala pengorbanannya.`,
     image: ""
@@ -37,7 +36,7 @@ Pada setiap 18 Maret, selain akan kita peringati sebagai hari kelahiranmu, akan 
     text: `Isi cerita usia empat.`,
     image: ""
   },
-   {
+  {
     age: 5,
     year: 2029,
     title: "Lima tahun pertama.",
